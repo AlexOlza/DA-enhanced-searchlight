17eb17b5a4faee746e1afae4b1330a1088212298
AlexOlza <alexander.olza@ehu.eus>
2025-03-28 17:40:56 +0100

    correct directory of file

--- DA_searchlight/do_searchlight_perception.sh
+++ DA_searchlight/do_searchlight_perception.sh
@@ -5,6 +5,6 @@ echo 'SUBJECT :' $i
 for (( j=0; j<4; j++ )); # for each subject (only first n_subj done here)
 do
 echo 'radius index :' $j
-sbatch ../send_sh_job.sh $1 $i 100 $2 $j #1 is the searchlight python script, 2 is 0 or 1 for the DA algorithm
+sbatch send_sh_job.sh $1 $i 100 $2 $j #1 is the searchlight python script, 2 is 0 or 1 for the DA algorithm
 done
 done
