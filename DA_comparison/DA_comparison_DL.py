b968ee4be003227e8f8684fc897e1093fd351b0c
AlexOlza <alexander.olza@ehu.eus>
2025-04-11 10:09:25 +0200

    correct usage of adapt

--- DA_comparison/DA_comparison_DL.py
+++ DA_comparison/DA_comparison_DL.py
@@ -16,6 +16,7 @@ Usage: python DA_comparison_DL.py source_domain:str target_domain:str subject:in
 """ THIRD PARTY IMPORTS """
 import sys
 import os
+# os.environ['TF_USE_LEGACY_KERAS']='True'
 sys.path.append('..')
 import logging
 logging.getLogger("tensorflow").setLevel(logging.ERROR)
@@ -30,6 +31,7 @@ import tensorflow as tf
 from adapt.feature_based import DeepCORAL, DANN, MCD
 from adapt.parameter_based import FineTuning
 from sklearn.metrics import balanced_accuracy_score
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import StratifiedGroupKFold
 from tqdm import tqdm
 import warnings
@@ -47,36 +49,63 @@ from dataManipulation.loadData import MyFullDataset
 #%%
 
 # Define networks
-from tensorflow.keras.layers import Dense
-class SequentialModule(tf.keras.Model):
-  def __init__(self, input_dim, output_dim, output_activation=tf.keras.activations.sigmoid, name=None):
-    super().__init__(name=name)
-    self.input_dim = input_dim
-    self.output_dim=output_dim
-    self.dense_1 = Dense(100,input_dim=input_dim, activation='relu')
-    self.dense_2 = Dense(10, activation='relu')
-    self.output_layer = Dense(output_dim, activation=output_activation)
-  def __call__(self, x, training=False):
-    x = self.dense_1(x)
-    x = self.dense_2(x)
-    return self.output_layer(x)
+# from tensorflow.keras.layers import Dense, ReLU, Softmax, Activation, Flatten
+# class SequentialModule(tf.keras.Model):
+#   def __init__(self, input_dim, output_dim, output_activation=tf.keras.activations.sigmoid, name=None):
+#     super().__init__(name=name)
+#     self.input_dim = input_dim
+#     self.output_dim=output_dim
+#     self.dense_1 = Dense(100,input_dim=input_dim, activation='relu')
+#     self.dense_2 = Dense(10, activation='relu')
+#     self.output_layer = Dense(output_dim, activation=output_activation)
+#   def __call__(self, x, training=False):
+#     x = self.dense_1(x)
+#     x = self.dense_2(x)
+#     return self.output_layer(x)
   # def summary(self):
   #       super().summary(self)
-class Encoder_model(SequentialModule):
-    def __init__(self,input_dim, output_dim=10, output_activation=tf.keras.activations.relu, name='enc'):
-      super().__init__(input_dim, output_dim,output_activation, name)
-# DANN needs a discriminator. The documentation states that 
-# "the output shape of the discriminator should be (None, 1) and a sigmoid activation should be used."
-# (here: https://adapt-python.github.io/adapt/generated/adapt.feature_based.DANN.html)
-class Discriminator_model(SequentialModule):
-    def __init__(self,input_dim, output_dim=1, output_activation=tf.keras.activations.sigmoid, name='disc'):
-      super().__init__(input_dim, output_dim,output_activation, name)
+# class Encoder_model(SequentialModule):
+#     def __init__(self,input_dim, output_dim=10, output_activation=tf.keras.activations.relu, name='enc'):
+#       super().__init__(input_dim, output_dim,output_activation, name)
+# # DANN needs a discriminator. The documentation states that 
+# # "the output shape of the discriminator should be (None, 1) and a sigmoid activation should be used."
+# # (here: https://adapt-python.github.io/adapt/generated/adapt.feature_based.DANN.html)
+# class Discriminator_model(SequentialModule):
+#     def __init__(self,input_dim, output_dim=1, output_activation=tf.keras.activations.sigmoid, name='disc'):
+#       super().__init__(input_dim, output_dim,output_activation, name)
 
-class Task_model(SequentialModule):
-    def __init__(self,input_dim=10, output_dim=1, output_activation=tf.keras.activations.relu, name='task'):
-      super().__init__(input_dim, output_dim,output_activation, name)
+# class Task_model(SequentialModule):
+#     def __init__(self,input_dim=10, output_dim=1, output_activation=tf.keras.activations.relu, name='task'):
+#       super().__init__(input_dim, output_dim,output_activation, name)
 
+# def make_encoder_model(n_voxels):
+#     model = tf.keras.Sequential()
+#     model.add(tf.keras.Input(shape=(n_voxels,)))  # more idiomatic than input_shape in first Dense
+#     model.add(Dense(100))
+#     model.add(ReLU())
+#     model.add(Dense(10))
+#     model.add(ReLU())
+#     model.add(Flatten())
+#     return model
 
+# def make_discriminator_model(n_voxels):
+#     model = tf.keras.Sequential()
+#     model.add(tf.keras.Input(shape=(n_voxels,)))  # more idiomatic than input_shape in first Dense
+#     model.add(Dense(100))
+#     model.add(ReLU())
+#     model.add(Dense(10))
+#     model.add(Dense(1))
+#     model.add(Activation("sigmoid"))
+#     return model
+
+# def make_task_model(n_voxels):
+#     model = tf.keras.Sequential()
+#     model.add(tf.keras.Input(shape=(n_voxels,)))  # more idiomatic than input_shape in first Dense
+#     model.add(Dense(100))
+#     model.add(ReLU())
+#     model.add(Dense(10))
+#     model.add(Softmax())
+#     return model
 #%%
 """ VARIABLE DEFINITION """
 source_domain =  sys.argv[1]
@@ -115,14 +144,7 @@ NITER_ = int(eval(sys.argv[6]))
 splitting='StratifiedGroupKFold'
 n_folds = 5
 ICA=True
-#%%
-def launch_priscilla():
-    i=0
-    for s in range(5):
-            for m, method in enumerate(method_names):
-                print(f'sbatch --job-name={s}{method[:3]} ../../main/raw.sh DA_comparison_DL.py perception imagery {s} {method} VC 100 1' )
-                i+=1
-    print('Total number of jobs: ', i)
+
 #%%
 fulldf=pd.DataFrame()
 
@@ -144,10 +166,10 @@ DA_method =  methods[method]
 params = parameters[method]
 
 REDUCE_DIM_NAME = ['NONE', 'TSVD', 'SRP', 'ICA'][REDUCE_DIM]
-result_filename = os.path.join(outdir, f'DA_{method}_reduce{REDUCE_DIM_NAME}.csv')
+result_filename = os.path.join(outdir, f'DA_DL_{method}_reduce{REDUCE_DIM_NAME}.csv')
 print('WILL PRODUCE FILE: ', result_filename)
 print(f'Fitting {method} for subject {subject} in region {region_name}...')
-Nts=range(10,110, 10) if dataset=='own' else [200, 250, 300]
+Nts=range(10,20, 10) if dataset=='own' else [200, 250, 300]
 if not Path(result_filename).is_file():
     remove_noise=True
     Source_X, Source_y, Source_g = MyFullDataset(source_domain, subject, region, 
@@ -172,8 +194,8 @@ if not Path(result_filename).is_file():
         else:
             Source, Target = DomainAdaptationGOD(Source_X, Target_X, Source_y, Target_y, Source_g, Target_g).split()
         d = DomainAdaptationData(Source, Target) #Just a wrapping class for convenience.
-        prediction_fname =os.path.join(outdir, f'{method}_preds_{Nt}_reduce{REDUCE_DIM_NAME}.csv')
-        NITER =len(d.Target_train_y)
+        prediction_fname =os.path.join(outdir, f'DL_{method}_preds_{Nt}_reduce{REDUCE_DIM_NAME}')
+        NITER =min(NITER_, len(d.Target_train_y))
         prediction_matrix =-1 *np.ones((len(Target_y),NITER))
         for i in tqdm(range(NITER)):
             
@@ -183,24 +205,26 @@ if not Path(result_filename).is_file():
             test_label = np.ravel(d.Source_test_y[i])
             
             # print('Original dataset shape %s' % Counter(train_label))
-            ros = RandomOverSampler(random_state=i)
-            if living:
-                train_label[train_label!=1]=0
-                test_label[test_label!=1]=0
-            # print('Original dataset shape %s' % Counter(train_label))
-            ros = RandomOverSampler(random_state=i)
+            # ros = RandomOverSampler(random_state=i)
+            # if living:
+            #     train_label[train_label!=1]=0
+            #     test_label[test_label!=1]=0
+            # # print('Original dataset shape %s' % Counter(train_label))
+            # ros = RandomOverSampler(random_state=i)
            
-            if oversample: train, train_label = ros.fit_resample(train, train_label)
+            # if oversample: train, train_label = ros.fit_resample(train, train_label)
             # print('Resampled dataset shape %s' % Counter(train_label))
             # We select a number "Nt" of instances from the target domain (usually Targetery)
-            I_train, I_test, IL_train, IL_test = d.Target_train_X[i], d.Target_test_X[i], d.Target_train_y[i], d.Target_test_y[i]
+            tr_idx = d.Target_train_i[i]
+            te_idx =  d.Target_test_i[i]
+            I_train, I_test, IL_train, IL_test = Target_X[tr_idx], Target_X[te_idx], Target_y[tr_idx], Target_y[te_idx]
             # I_train contains "Nt" instances. Those are passed to the ADAPT method
             I_test_idx =d.Target_test_i[i]
             if living:
                 IL_train[IL_train!=1]=0
                 IL_test[IL_test!=1]=0
             
-            if oversample: I_train, IL_train = ros.fit_resample(I_train, IL_train)
+            # if oversample: I_train, IL_train = ros.fit_resample(I_train, IL_train)
             # print('Resampled target dataset shape %s' % Counter(IL_train))
             
             if REDUCE_DIM!=0:
@@ -209,32 +233,34 @@ if not Path(result_filename).is_file():
             n_voxels = train.shape[-1]
             
             if method=='FineTuning':
-                encoder = Encoder_model(n_voxels,10)
-                task = Task_model(n_voxels)
-                discriminator = Discriminator_model(n_voxels)
-                FineTuning_model = FineTuning(encoder, task,train, train_label)
-                FineTuning_model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=False),
+                # encoder = Encoder_model(n_voxels,10)
+                # task = Task_model(n_voxels)
+                # task = get_task(units=N_classes)
+                # discriminator = Discriminator_model(n_voxels)
+                FineTuning_model = FineTuning(#encoder, task,
+                                              # task=task
+                                              # X=train, y=train_label
+                                              )
+                FineTuning_model.compile(
+                    # loss=tf.keras.losses.CategoricalCrossentropy(from_logits=False),
                                    optimizer=tf.keras.optimizers.legacy.Adam(0.001), 
                                    metrics=["accuracy"]
                                    )
                                    
                 # Train FineTuning
-                FineTuning_model.fit(train, train_label, batch_size=64, epochs=100,
-                                     validation_data=(I_train,IL_train),verbose=0
+                FineTuning_model.fit(train, train_label, 
+                                     # batch_size=64,
+                                     epochs=100,
+                                     # validation_data=(I_train,IL_train),
+                                     verbose=0
                               )
-                # ft.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=False),
-                #                    optimizer=tf.keras.optimizers.legacy.Adam(0.001), 
-                #                    metrics=["accuracy"]
-                #                    )
-                                   
-                # # Train FineTuning
-                # ft.fit(train, train_label, batch_size=64, epochs=100,
-                #                      validation_data=(I_train,IL_train),verbose=0
-                #               )
+
                 #%%
 
-                clf = FineTuning(encoder=FineTuning_model.encoder_, loss=tf.keras.losses.BinaryCrossentropy(from_logits=False),
-                                       pretrain=True,  pretrain__epochs=30, random_state=random_state,
+                clf = FineTuning(encoder=FineTuning_model.encoder_,
+                                 # loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+                                       pretrain=False,#True,  
+                                       pretrain__epochs=30, random_state=random_state,
                                        optimizer=tf.keras.optimizers.legacy.Adam(0.001),
                                        optimizer_enc=tf.keras.optimizers.legacy.Adam(0.0001),
                                        metrics = ['accuracy'])
@@ -256,87 +282,75 @@ if not Path(result_filename).is_file():
                 # plt.legend(ncol=2)
                 # plt.show()
             elif method=='DANN':
-                encoder = Encoder_model(n_voxels,10)
-                task = Task_model(n_voxels)
-                discriminator = Discriminator_model(n_voxels)
-                clf = DANN(encoder, task,discriminator)
+                # encoder = Encoder_model(n_voxels,10)
+                # task = Task_model(n_voxels)
+                # discriminator = Discriminator_model(n_voxels)
+                # encoder = make_encoder_model(n_voxels)
+                # discriminator = make_discriminator_model(n_voxels)
+                # task = make_task_model(n_voxels)
+                clf = DANN()#encoder=encoder, discriminator=discriminator)
                 clf.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=False),
                                     optimizer=tf.keras.optimizers.legacy.Adam(0.001),
                                     optimizer_enc=tf.keras.optimizers.legacy.Adam(0.0001),
                                     metrics=["accuracy"])
                                    
                 # Train DANN
-                clf.fit(X=train,y=train_label, Xt=I_train,batch_size=64, epochs=500,
+                clf =clf.fit(X=train,y=train_label, Xt=I_train,batch_size=64, epochs=500,
                             verbose=0 )
-                # print("Evaluate")
-                # result = clf.predict(I_train)
-                # pred_classes = np.where(result.ravel()>=0.5,1,0)
-                # print(balanced_accuracy_score(pred_classes, IL_train))
-                # result = clf.predict(I_test)
-                # pred_classes = np.where(result.ravel()>=0.5,1,0)
-                # print(balanced_accuracy_score(pred_classes, IL_test))
-                # import pandas as pd
-                # from matplotlib import pyplot as plt
-                # pd.DataFrame(clf.history_).plot(figsize=(8, 5))
-                # plt.title("Training history", fontsize=14); plt.xlabel("Epochs"); plt.ylabel("Scores")
-                # plt.legend(ncol=2)
-                # plt.show()
+
 
             elif method=='DeepCORAL':
-                encoder = Encoder_model(n_voxels,10)
-                task = Task_model(n_voxels)
-                clf = DeepCORAL(encoder, task,Xt=I_train, metrics=["accuracy"],
+                # encoder = Encoder_model(n_voxels,10)
+                # task = Task_model(n_voxels)
+                clf = DeepCORAL(#encoder, task,
+                                Xt=I_train, metrics=["accuracy"],
                                   optimizer=tf.keras.optimizers.legacy.Adam(0.001),
                                   optimizer_enc=tf.keras.optimizers.legacy.Adam(0.0001),
                                   random_state=random_state)
 
                 clf.fit(X=train,y=train_label, epochs=200, verbose=0)
-                # print("Evaluate")
-                # result = clf.predict(I_train)
-                # pred_classes = np.where(result.ravel()>=0.5,1,0)
-                # print(balanced_accuracy_score(pred_classes, IL_train))
-                # result = clf.predict(I_test)
-                # pred_classes = np.where(result.ravel()>=0.5,1,0)
-                # print(balanced_accuracy_score(pred_classes, IL_test))
-                # import pandas as pd
-                # from matplotlib import pyplot as plt
-                # pd.DataFrame(clf.history_).plot(figsize=(8, 5))
-                # plt.title("Training history", fontsize=14); plt.xlabel("Epochs"); plt.ylabel("Scores")
-                # plt.legend(ncol=2)
-                # plt.show()
 
             elif method=='MCD':
-                encoder = Encoder_model(n_voxels,10)
-                task = Task_model(n_voxels)
-                discriminator = Discriminator_model(n_voxels)
-                clf = MCD(encoder, task,Xt=I_train, metrics=["accuracy"],
+                # def get_task(activation="sigmoid", units=N_classes):
+                #     model = tf.keras.Sequential()
+                #     model.add(Flatten())
+                #     model.add(Dense(10, activation="relu"))
+                #     model.add(Dense(10, activation="relu"))
+                #     model.add(Dense(units, activation=activation))
+                #     return model
+                # encoder = Encoder_model(n_voxels,10)
+                # task = Task_model(n_voxels, output_dim=N_classes)
+                # task = get_task(units=N_classes)#make_task_model(n_voxels)
+                # discriminator = Discriminator_model(n_voxels)#.build(1,n_voxels)
+                # loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False)
+                clf = MCD(Xt=I_train, metrics=["accuracy"], 
+                          # task=task,
+                          # encoder = encoder,
+                              # loss='categorical_crossentropy',#loss,
                                   optimizer=tf.keras.optimizers.legacy.Adam(0.001),
                                   optimizer_enc=tf.keras.optimizers.legacy.Adam(0.0001),
                                   random_state=random_state)
 
-                clf.fit(X=train,y=train_label, epochs=200, verbose=0)  
-                # print("Evaluate")
-                # result = clf.predict(I_train)
-                # pred_classes = np.where(result.ravel()>=0.5,1,0)
-                # print(balanced_accuracy_score(pred_classes, IL_train))
-                # result = clf.predict(I_test)
-                # pred_classes = np.where(result.ravel()>=0.5,1,0)
-                # print(balanced_accuracy_score(pred_classes, IL_test))
-                # import pandas as pd
-                # from matplotlib import pyplot as plt
-                # pd.DataFrame(clf.history_).plot(figsize=(8, 5))
-                # plt.title(f"Training history ({clf.count_params()} parameters)", fontsize=14); plt.xlabel("Epochs"); plt.ylabel("Scores")
-                # plt.legend(ncol=2)
-                # plt.show()
+                clf.fit(X=train,y=train_label,
+                        # Xt=I_train, yt=IL_train, 
+                        epochs=100, verbose=0)  
+                
 
             else:
                 assert False, 'Unrecognised DA method'     
             
-           
-            aux_ys = np.where(clf.predict(test).ravel()>=0.5 ,1,0)               # Predictions in source domain 
-            aux_ys_imag = np.where(clf.predict(I_test).ravel()>=0.5 ,1,0)         # Predictions in target domain
-            aux_ys_imag_tr =np.where( clf.predict(I_train).ravel()>=0.5 ,1,0)
-                
+            train_enc = clf.encoder_.predict(train)
+            # train_enc__ = clf.transform(train)
+            I_train_enc = clf.encoder_.predict(I_train)
+            test_enc = clf.encoder_.predict(test)
+            I_test_enc = clf.encoder_.predict(I_test)
+            
+            lr = LogisticRegression().fit(train_enc, train_label)
+            
+            aux_ys = lr.predict(test_enc)             # Predictions in source domain 
+            aux_ys_imag = lr.predict(I_test_enc)       # Predictions in target domain
+            aux_ys_imag_tr = lr.predict(I_train_enc)
+            print(np.unique(aux_ys))
             balanced_accuracy_s[i]=balanced_accuracy_score( test_label, aux_ys)
             
             balanced_accuracy_im_s[i]=balanced_accuracy_score( IL_test, aux_ys_imag)
@@ -359,4 +373,3 @@ if not Path(result_filename).is_file():
 else:
     print('Nothing done: The result file already exists.')
 
-#%%
